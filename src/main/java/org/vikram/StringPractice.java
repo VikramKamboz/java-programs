135458dd9f2259bca18e11c3c350c45d5855b05f
vikram <vikramkamboj14@gmail.com>
2024-09-16 13:16:46 +0530

    String practice program

--- src/main/java/org/vikram/StringPractice.java
+++ src/main/java/org/vikram/StringPractice.java
@@ -6,9 +6,12 @@ import java.util.stream.Stream;
 
 public class StringPractice {
     public static void main(String[] args) {
-        reverseStringInSameOrder();
-        charCountStringUsingCharArray();
-        charCountString();
+       // reverseStringInSameOrder();
+        //charCountStringUsingCharArray();
+        //charCountString();
+        reverseString();
+        reverseAaString();
+
 
 
     }
@@ -48,7 +51,72 @@ public class StringPractice {
                 map.put(input.charAt(i), 1);
             }
         }
-
         map.entrySet().stream().forEach(m -> System.out.println("map values : "  + m.getKey() +" " +  m.getValue()));
+    }
+
+    public static Map<Character, Integer> countChar(String input) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i=0; i<input.length(); i++) {
+            if(map.containsKey(input.charAt(i))) {
+                map.put(input.charAt(i), map.get(input.charAt(i)) +1 );
+            } else {
+                map.put(input.charAt(i), 1);
+            }
+        }
+       return map;
+    }
+
+    public static void reverseString() {
+        String input = "reverse a string";
+        String inputArr[] = input.split(" ");
+        StringBuilder output = new StringBuilder();
+        for (String word : inputArr) {
+            Map<Character, Integer> map = countChar(word);
+            if (map.getOrDefault('r', 0) == 2) {
+                output.append(new StringBuilder(word).reverse().toString());
+            } else {
+                output.append(word);
+            }
+            output.append(" ");
+        }
+        System.out.println("reverse : " + output);
+    }
+
+    public static void reverseAString() {
+        String input = "reverse a string";
+        String inputArr[] = input.split(" ");
+        StringBuilder output = new StringBuilder();
+        for (String word : inputArr) {
+            Map<Character, Integer> map = countChar(word);
+            int countOfR = map.containsKey('r') ? map.get('r') : 0; // Get the count of 'r' by  ternary operator to check whether the map contains the key 'r' and then assigns a value to countOfR accordingly.
+
+            if (countOfR == 2) {
+                output.append(new StringBuilder(word).reverse());
+            } else {
+                output.append(word);
+            }
+            output.append(" "); // Add space between words
+        }
+        System.out.println("reverse : " + output);
+    }
+
+    public static void reverseAaString() {
+        String input = "reverse a string";
+        String inputArr[] = input.split(" ");
+        StringBuilder output = new StringBuilder();
+        for (String word : inputArr) {
+            Map<Character, Integer> map = countChar(word);
+            int countOfR =0;
+            if(map.containsKey('r')) {
+                countOfR = map.get('r');
+            }
+            if (countOfR == 2) {
+                output.append(new StringBuilder(word).reverse());
+            } else {
+                output.append(word);
+            }
+            output.append(" ");
+        }
+        System.out.println("reverse : " + output);
     }
 }
